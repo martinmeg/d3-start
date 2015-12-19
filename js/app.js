79ea01d8863546ca82f5a55466127b687b91263e
Martin Guignard <martinmeg.ar@gmail.com>
2015-12-19 12:07:57 -0300

    Added a list of data items

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var data = [
+	'Walter White',
+    'Skyler White',
+    'Jesse Pinkman',
+    'Hank Schrader',
+    'Marie Schrader',
+    'Walter White Jr.',
+    'Saul Goodman',
+    'Gustavo Fring',
+    'Mike Ehrmantraut',
+    'Lydia Rodarte-Quayle',
+    'Todd Alquist'
+];
+
+d3.select('#bb-list')
+	.selectAll('li')
+	.data(data)
+	.enter()
+	.append('li')
+	.text(function(d) {
+		return d;
+	});
